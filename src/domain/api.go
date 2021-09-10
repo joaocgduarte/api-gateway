ad8e916b4ebdfdf4fb60fc6c3d8349f7fdd1c87c
João Duarte <joaocgarciaduarte@gmail.com>
2021-09-10 16:53:52 +0100

    feat(validator): add validator package
    This package allows us to easily validate struct and variable values

--- src/domain/api.go
+++ src/domain/api.go
@@ -1,21 +1,25 @@
 package domain
 
-import "context"
+import (
+	"context"
+
+	"github.com/plagioriginal/api-gateway/protos/protos"
+)
 
 type LoginRequest struct {
-	Username string
-	Passwor  string
+	Username string `validate:"required,min=3"`
+	Password string `validate:"required,min=8"`
 }
 
 type AddUserRequest struct {
-	Username string
-	Password string
-	Role     string
-	JwtToken string
+	Username string `validate:"required,min=3"`
+	Password string `validate:"required,min=3"`
+	Role     string `validate:"required"`
+	JwtToken string `validate:"required"`
 }
 
 type ListProjectsRequest struct {
-	JwtToken string
+	JwtToken string `validate:"required"`
 	PageNum  uint
 }
 
@@ -24,8 +28,8 @@ type ListProjectsRequest struct {
 //
 // @todo: define the `interfaces{}` with the Objects from the gRPC clients.
 type APIService interface {
-	Login(ctx context.Context, loginRequest LoginRequest) (interface{}, error)
-	RefreshJWT(ctx context.Context, refreshToken string) (interface{}, error)
-	AddUser(ctx context.Context, userRequest AddUserRequest) (interface{}, error)
+	Login(ctx context.Context, loginRequest LoginRequest) (*protos.TokenResponse, error)
+	RefreshJWT(ctx context.Context, refreshToken string) (*protos.TokenResponse, error)
+	AddUser(ctx context.Context, userRequest AddUserRequest) (*protos.UserResponse, error)
 	ListProjects(ctx context.Context, listProjectReq ListProjectsRequest) (interface{}, error)
 }
--- src/go.mod
+++ src/go.mod
@@ -4,9 +4,14 @@ go 1.17
 
 require (
 	github.com/go-chi/chi/v5 v5.0.4 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.9.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
-	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
 	golang.org/x/text v0.3.6 // indirect
 	google.golang.org/genproto v0.0.0-20210909211513-a8c4777a87af // indirect
 	google.golang.org/grpc v1.40.0 // indirect
